18502b6f6fbfdc1727e72025e66859ca23ef84c3
Satyam Rai <satyamrai553@gmail.com>
2025-01-01 03:49:34 +0530

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,3 +1,52 @@
+const {JSDOM} = require("jsdom")
+function getURLsFromHTML(htmlBody, baseURL){
+    const urls =[]
+    const dom = new JSDOM(htmlBody)
+    const linkELements = dom.window.document.querySelectorAll('a')
+    for(const linkELement of linkELements){
+        if(linkELement.href.slice(0,1)==='/'){
+            //relative
+           try {
+            const urlObj = new URL(`${baseURL}${linkELement.href}`)
+            urls.push(urlObj.href)
+           } catch (error) {
+            console.log(`Error with relative url: ${error.message}`)
+           }
+        }
+        else{
+            try {
+                const urlObj = new URL(`${linkELement.href}`)
+            urls.push(urlObj.href)
+            } catch (error) {
+                console.log(`Error with absolute url: ${error.message}`)
+            }
+        }
+      
+    }
+    return urls
+}
+
+async function crawlPage(currentURL){
+    console.log(`actively crawling: ${currentURL}`)
+    try {
+        const resp = await fetch(currentURL)
+        if(resp.status > 399){
+            console.log(`Error in fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return
+        }
+        const contentType = resp.headers.get("content-type")
+        if(!contentType.includes("text/html")){
+            console.log(`non html response, content type: ${contentType}, on page: ${currentURL}`)
+            return
+        }
+        console.log(await resp.text())
+    } catch (error) {
+        console.log(`Error in fetch: ${error.message} on page: ${currentURL}`)
+    }
+    
+}
+
+
 function normalizeURL(urlString){
     const urlObj = new URL(urlString)
     const hostPath =`${urlObj.hostname}${urlObj.pathname}`
@@ -10,5 +59,7 @@ function normalizeURL(urlString){
 }
 
 module.exports={
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML,
+    crawlPage
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require("./crawl.js")
+const {normalizeURL, getURLsFromHTML} = require("./crawl.js")
 const {test, expect} = require("@jest/globals") 
 
 
@@ -6,5 +6,53 @@ test("normalizeURL", ()=>{
     const input= "http://satyamcodes.online/home/"
     const actual = normalizeURL(input)
     const expected = "satyamcodes.online/home"
+    expect(actual).toEqual(expected)
+})
+
+
+
+test("getURLsFromHTML abosolute", ()=>{
+    const inputHTMLBody= `
+    <html>
+    <body>
+    <a href="http://satyamcodes.online">
+    Satyam </a>
+    </body>
+    </html>
+    `
+    const inputBaseURL = "http://satyamcodes.online/"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["http://satyamcodes.online/"]
+    expect(actual).toEqual(expected)
+})
+
+
+
+test("getURLsFromHTML relative", ()=>{
+    const inputHTMLBody= `
+    <html>
+    <body>
+    <a href="/path/">
+    Satyam </a>
+    </body>
+    </html>
+    `
+    const inputBaseURL = "http://satyamcodes.online"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["http://satyamcodes.online/path/"]
+    expect(actual).toEqual(expected)
+})
+test("getURLsFromHTML invalid url", ()=>{
+    const inputHTMLBody= `
+    <html>
+    <body>
+    <a href="invalid">
+    invalid url</a>
+    </body>
+    </html>
+    `
+    const inputBaseURL = "http://satyamcodes.online"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = []
     expect(actual).toEqual(expected)
 })
